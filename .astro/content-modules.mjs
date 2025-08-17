b82b7a1e23264fd9b38dc3bb2e4747e16049ea98
Nikita Volodarskiy <volodarskyns@gmail.com>
2025-08-17 12:25:32 +0400

    New posts

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,6 @@
 
 export default new Map([
+["src/content/posts/17-August-2025/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F17-August-2025%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/pages/about/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fabout%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/posts/17-August-2025/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F17-August-2025%2Findex.mdx&astroContentModuleFlag=true")]]);
+["src/content/posts/16-August-2025/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F16-August-2025%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
