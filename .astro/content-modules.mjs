3a47cfb4354cf8eb7e1d090be2e1590f2562a721
Nikita Volodarskiy <volodarskyns@gmail.com>
2025-08-17 16:09:34 +0400

    New Favicon
    Need to spend some time on designing a better one in future. But it is what it its.

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,6 @@
 
 export default new Map([
-["src/content/posts/17-August-2025/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F17-August-2025%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/pages/about/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fabout%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/posts/16-August-2025/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F16-August-2025%2Findex.mdx&astroContentModuleFlag=true")]]);
+["src/content/posts/1-web-app-music-catalog/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F1-web-app-music-catalog%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/posts/2-chatgpt-mvp-rocket/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F2-chatgpt-mvp-rocket%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
