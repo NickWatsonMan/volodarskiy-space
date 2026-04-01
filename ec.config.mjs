056a1adc92b8b90a652c547f5cc952bcf7394cc7
Nikita Volodarskiy <volodarskyns@gmail.com>
2026-04-01 11:32:15 +0200

    _

--- ec.config.mjs
+++ ec.config.mjs
@@ -2,4 +2,7 @@ import { defineEcConfig } from "astro-expressive-code";
 
 export default defineEcConfig({
   themes: ["github-light", "github-dark"],
+  useDarkModeMediaQuery: false,
+  themeCssRoot: "body",
+  themeCssSelector: (theme) => (theme.type === "dark" ? ".dark" : false),
 });
