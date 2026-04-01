1612f582e410f7cdf702bf77c9666e0b62a985a2
Nikita Volodarskiy <volodarskyns@gmail.com>
2026-04-01 11:11:44 +0200

    Fixing code style again

--- ec.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineEcConfig } from "astro-expressive-code";
-
-export default defineEcConfig({
-  themes: ["github-light", "kanagawa-wave"],
-  styleOverrides: {
-    borderRadius: "var(--radius-m)",
-  },
-});
